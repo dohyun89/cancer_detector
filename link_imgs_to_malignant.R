ecba311be33481a88eaf32a058cd0b3a99a1aba0
Do Hyun Hwang <dohyun89@gmail.com>
2019-12-07 11:16:16 -0600

    fixed typo

--- link_imgs_to_malignant.R
+++ link_imgs_to_malignant.R
@@ -1,6 +1,6 @@
 path_malignant =  paste0(getwd(),'/Skin Cancer Data/malignant/' ,list.files(path = './Skin Cancer Data/malignant', 
                                                                             pattern = '.jpg'))
-path_benign = paste0(getwd(),'/Skin Cancer Data/benign', list.files(path = './Skin Cancer Data/benign', 
+path_benign = paste0(getwd(),'/Skin Cancer Data/benign/', list.files(path = './Skin Cancer Data/benign', 
                                                                     pattern = '.jpg'))
 
 df_file= data.frame("filepath"= c(path_malignant,path_benign), 
