87f405184a6235bd38c9bb5ec75a5462df1c7d6b
Do Hyun Hwang <dohyun89@gmail.com>
2019-12-09 01:11:33 -0600

    no need to open images to find dimension

--- link_imgs_to_malignant.R
+++ link_imgs_to_malignant.R
@@ -11,19 +11,4 @@ df_file= data.frame("filepath"= c(path_malignant, path_benign),
                     "file_name" = c(malignant_name, benign_name),
                     "malignant" =  c(rep(1,150), rep(0,150)), stringsAsFactors = FALSE)
 
-temp_path = c()
-temp_dim_1 = c()
-temp_dim_2 = c()
-for (i in df_file$filepath)
-{
-  temp = load.image(i)
-  temp_path = append(temp_path, i)
-  temp_dim_1 = append(temp_dim_1, dim(temp)[1])
-  temp_dim_2 = append(temp_dim_2, dim(temp)[2])
-  
-}
-df_dim = data.frame("filepath" = temp_path, "dim_1" = temp_dim_1, "dim_2" = temp_dim_2, stringsAsFactors = FALSE)
-
-df_file = merge(df_file, df_dim, by = "filepath")
-
 write.csv(df_file, 'original_file_map.csv', row.names = FALSE)
