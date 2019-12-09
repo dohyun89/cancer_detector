4c4e067c62657825563ce0f9cf61520a750295c3
Do Hyun Hwang <dohyun89@gmail.com>
2019-12-09 00:13:03 -0600

    small revision

--- create_df_flat_img.R
+++ create_df_flat_img.R
@@ -1,18 +1,27 @@
+## This module will read the resized images, and create a dataframe
+## with flattened images as a row
+## It is recommended to set your working directory to the source code location.
+
+path_to_benign_images = './resize/benign/' #Be sure to end with '/'
+path_to_malignant_images = './resize/malignant/' #Be sure to end with '/'
+
 file_map =  read.csv('original_file_map.csv', stringsAsFactors = FALSE)
 file_map = file_map[,-1]
 file_map[ , c('filepath','dim_1', 'dim_2')] = NULL
 file_name = c()
 img = c()
 
+
+
 count = 0
 for(i in file_map$file_name)
 {
   if(file_map[file_map$file_name ==i,]$malignant == 0)
   {
-    path = paste0('./resize/benign/', i)
+    path = paste0(path_to_benign_images, i)
   }else
   {
-    path = paste0('./resize/malignant/', i)
+    path = paste0(path_to_malignant_images, i)
   }
   img = rbind(img, c(load.image(path)) )
   file_name = cbind(file_name,i)
